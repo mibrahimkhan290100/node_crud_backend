093f5d74747adeea9b24b242d5a5326f6ecfec91
ibrahimssquare768 <ibrahim@ssquare.pk>
2025-06-18 12:09:54 +0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use('/' ,  (req , res) =>{
+    res.send("Api is running in Background");
+})
+
+module.exports = app
